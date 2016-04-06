7b5bac941e029c52c41505b7c294a7a81110c2e7
Saswat Padhi <saswat.sourav@gmail.com>
2016-04-06 01:16:01 -0700

    Added Byobu

---
+++ byobu/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BYOBU="byobu"
+BYOBU_CONFIG_PATH="${HOME}/.byobu"
+
+# ------------------------------------------------------------------------------
+
+__BASH_BYOBU_INSTALL_SH_SOURCE__="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__BASH_BYOBU_INSTALL_SH_LIB__="${__BASH_BYOBU_INSTALL_SH_SOURCE__}/../_lib"
+
+source "${__BASH_BYOBU_INSTALL_SH_LIB__}/depends.sh"
+source "${__BASH_BYOBU_INSTALL_SH_LIB__}/logging.sh"
+source "${__BASH_BYOBU_INSTALL_SH_LIB__}/replace.sh"
+
+verify_dependencies "-V" "${BYOBU}"
+
+# ------------------------------------------------------------------------------
+
+print_border HEADING "byobu config installation"
+
+# Link status file
+
+log INFO "[+] Linking status file\n"
+check_and_replace "${BYOBU_CONFIG_PATH}/status" allow_backup
+if [ "$?" -eq $BACKUP_CODE ]; then EXTRA_FLAGS="--backup=numbered" ; fi
+log INFO ; ln -rfsv $EXTRA_FLAGS "${__BASH_BYOBU_INSTALL_SH_SOURCE__}/status" "${BYOBU_CONFIG_PATH}/status"
+
+# Link tmux config file
+
+log INFO "[+] Linking tmux config file\n"
+check_and_replace "${BYOBU_CONFIG_PATH}/.tmux.conf" allow_backup
+if [ "$?" -eq $BACKUP_CODE ]; then EXTRA_FLAGS="--backup=numbered" ; fi
+log INFO ; ln -rfsv $EXTRA_FLAGS "${__BASH_BYOBU_INSTALL_SH_SOURCE__}/.tmux.conf" "${BYOBU_CONFIG_PATH}/.tmux.conf"
+
+print_border TAILING "byobu config installation"
